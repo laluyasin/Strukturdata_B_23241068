837def8a8bdd77bd8b0478302f228f8b67a3729e
laluyasin <146050764+laluyasin@users.noreply.github.com>
2024-04-01 12:00:27 +0800

    senin_01,April,2024

---
+++ strukturdata/meet2.py
@@ -0,0 +1,21 @@
+# belajar variabel
+# assigment
+x = 3
+y = 6
+uang = 1000
+
+# pemanggilan 
+print ("Nilai UAS saya =", x)
+print ("Nilai UTS saya =", y)
+print ("sisa uang saya =", uang)
+
+# pemenang variabel 
+nilai_a = 9
+kotak10 = 10
+
+#input variabel kedua 
+x = 30 
+print ("Nilai UAS saya =", x)
+x = 3000
+print ("Nilai UAS saya =", x)
+
