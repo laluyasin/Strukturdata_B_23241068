69903155a35b414e54561c2d2baf3bd0f1a3efe0
laluyasin <146050764+laluyasin@users.noreply.github.com>
2024-04-22 11:55:15 +0800

    senin/22/april/2024

---
+++ struktur data/meet3.py
@@ -0,0 +1,22 @@
+# contoh tipe data (integer) : angka satuan 
+bil_integer = 1
+print(type( bil_integer))
+
+print ("type data dari bil_integer adalah: ", type (bil_integer))
+# contoh tipe data (float) : bilangan desimal 
+bil_float = 2.5
+print ("type data bil_float adalah: ", type (bil_float))
+
+# contoh tipe data (string) : kumpulan kata karekter 
+bil_string = "2.5"
+print ("type data bil_float adalah: ", type (bil_string))
+
+# contoh tipe data (bvolean) : biner, 0,1 True false 
+bil_bol = True 
+print ("type data dari bolean_data adalah:", type(bil_bol))
+
+# tipe data dari bahasa c
+from ctypes import c_double 
+bil_double = c_double(20000000)
+print ("type data  bil_double adalah: ", type (bil_double))
+
