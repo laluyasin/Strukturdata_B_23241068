28ef63d5c0b2f1fce4ab1a65916ceca9d7e33b92
laluyasin <146050764+laluyasin@users.noreply.github.com>
2024-06-03 10:59:23 +0700

    senin 3/juni/2024

---
+++ strukturdata meet9/meet9.py
@@ -0,0 +1,25 @@
+# percabangan jika maka
+
+name = input ("what is yoour name :")
+print (name)
+
+# if kondisi : aksi
+
+#if standarif 
+#if name == "mas'ud" : print ("player hoki")
+#print ("end of program")
+
+
+# if identitas 
+#if name =="mas'ud" :
+#    print ("player hoki")
+#    print ("hoki banget")
+#print ("end of program")
+
+# if else 
+if name =="rawwr" :
+    print ("player hoki")
+    print ("hoki banget")
+else:
+    print ("anda tidak hoki")
+print ("end of program")
